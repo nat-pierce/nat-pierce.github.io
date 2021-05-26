6fff4054f42915ab65f9032cb0b2fc43e69c1a31
nat <nat.r.pierce@gmail.com>
2021-05-26 18:56:20 -0400

    landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useEffect, useRef } from 'react';
 import './App.scss';
+import LandingSection from './LandingSection';
 
 function App() {
+    const containerRef = useRef(null);
+
+    const updateWindowDimensions = () => {
+        const height = window.innerHeight;
+
+        containerRef.current.style.height = `${height}px`;
+    }
+
+    useEffect(() => {
+        updateWindowDimensions();
+
+        window.addEventListener('resize', updateWindowDimensions);
+
+        return () => {
+            window.removeEventListener('resize', updateWindowDimensions);
+        }
+    }, []);
+
     return (
-        <div className="app-container">
-            Test
+        <div className="app-container" ref={containerRef}>
+            <LandingSection />
         </div>
     );
 }
---
+++ src/LandingSection/index.js
@@ -0,0 +1,17 @@
+import Particles from "react-tsparticles";
+import { particlesOptions } from "./ParticlesOptions";
+import './LandingSection.scss';
+
+export default function LandingSection() {
+    return (
+        <div className='landing-section'>
+            <Particles
+                options={particlesOptions}
+            />
+            <div className='text-section'>
+                <h1 className='name'>Natalie Pierce</h1>
+                <h3 className='job-title'>Senior Front-end Software Engineer</h3>
+            </div>
+        </div>
+    )
+}
