515274a3f0ec247abc73df81d64f16e80b5fbe05
nat-pierce <nat.r.pierce@gmail.com>
2024-08-22 15:30:16 -0400

    add bluma project

--- src/ProfileSection/index.js
+++ src/ProfileSection/index.js
@@ -1,14 +1,14 @@
 import './ProfileSection.scss';
-import me from '../Resources/Images/meWithBg.JPG';
+import me from '../Resources/Images/me2.jpg';
 
 export default function ProfileSection() {
     return (
         <div className='profile-section' id='profile-section'>
             <div className='text-section'>
                 <h1>Hi, I'm Nat.</h1>
-                <div>I'm a front-end software engineer that likes writing clean code and building beautiful user interfaces.</div>
+                <div>I'm a full-stack software engineer who enjoys writing clean code and building beautiful apps.</div>
                 <div>The intersection between art and technology constantly inspires me to learn new skills, technologies, and art forms.</div>
-                <div>When I'm not coding, you can find me in the dance studio or playing with my Corgi, Gunter.</div>
+                <div>When I'm not coding, you can find me in the dance studio, playing games, or hanging with my Corgi, Gunter.</div>
             </div>
             <img className='me' src={me} alt='me' />
         </div>
--- src/ProjectsSection/index.js
+++ src/ProjectsSection/index.js
@@ -1,54 +1,61 @@
 import './ProjectsSection.scss';
 import piqrQr from '../Resources/Images/piqr-qr.png';
 import WhatsYourListLogo from './WhatsYourListLogo';
+import KeetyImg from '../Resources/Images/keety.png';
+import BlumaLogo from '../Resources/Images/With_mascot.gif';
 
 export default function ProjectsSection() {
     return (
         <div className='projects-section' id='projects-section'>
-            <div className='top-row'>
-                <div className='project korio'>
-                    <div className='left'>
-                        <iframe 
-                            src="https://www.youtube.com/embed/ZwS21DYQLCE" 
-                            title="YouTube video player" 
-                            frameBorder="0" 
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                            allowFullScreen>
-                        </iframe>
-                    </div>
-                    <div className='right'>
-                        <a href="https://korio.studio" target="_blank" rel="noreferrer">Korio.Studio</a>
-                        <div className='subtitle'>The music player for dancers</div>
-                    </div>
+            <div className='project bluma'>
+                <div className='top'>
+                    <img src={BlumaLogo} />
                 </div>
-                <div className='project piqr'>
-                    <div className='left'>
-                        <img src={piqrQr} alt='piqr.me' />
-                    </div>
-                    <div className='right'>
-                        <a href="https://piqr.me" target="_blank" rel="noreferrer">piqr.me</a>
-                        <div className='subtitle'>Photo sharing made safe</div>
-                    </div>
+                <div className='bottom'>
+                    <a href="https://store.steampowered.com/app/3032520/BLUMA/" target="_blank" rel="noreferrer">BLUMA</a>
+                    <div className='subtitle'>Cozy puzzle-platforming adventure</div>
                 </div>
             </div>
-            <div className='bottom-row'>
-                <div className='project whats-your-list'>
-                    <div className='left'>
-                        <WhatsYourListLogo shouldAnimate={true} animationSpeed={1500} sizeScale={0.5} />
-                    </div>
-                    <div className='right'>
-                        <a href="https://whatsyourlist.com" target="_blank" rel="noreferrer">WhatsYourList.com</a>
-                        <div className='subtitle'>Share and compare your favorite movies with friends</div>
-                    </div>
+            <div className='project keety'>
+                <div className='top'>
+                    <img src={KeetyImg} />
                 </div>
-                <div className='project keety'>
-                    <div className='left'> </div>
-                    <div className='right'>
-                        <div className='text-wrapper'>
-                            <a href="https://apps.apple.com/us/app/keety/id1131054781" target="_blank" rel="noreferrer">Keety</a>
-                            <div className='subtitle'>Arcade action shooter based on cat memes</div>
-                        </div>
-                    </div>
+                <div className='bottom'>
+                    <a href="https://apps.apple.com/us/app/keety/id1131054781" target="_blank" rel="noreferrer">Keety</a>
+                    <div className='subtitle'>Arcade action shooter based on cat memes</div>
+                </div>
+            </div>
+            <div className='project korio'>
+                <div className='top'>
+                    <iframe 
+                        src="https://www.youtube.com/embed/ZwS21DYQLCE" 
+                        title="YouTube video player" 
+                        frameBorder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowFullScreen>
+                    </iframe>
+                </div>
+                <div className='bottom'>
+                    <a href="https://korio.studio" target="_blank" rel="noreferrer">Korio.Studio</a>
+                    <div className='subtitle'>The music player for dancers</div>
+                </div>
+            </div>
+            <div className='project whats-your-list'>
+                <div className='top'>
+                    <WhatsYourListLogo shouldAnimate={true} animationSpeed={1500} sizeScale={0.5} />
+                </div>
+                <div className='bottom'>
+                    <a href="https://whatsyourlist.com" target="_blank" rel="noreferrer">WhatsYourList.com</a>
+                    <div className='subtitle'>Share and compare your favorite movies with friends</div>
+                </div>
+            </div>
+            <div className='project piqr'>
+                <div className='top'>
+                    <img src={piqrQr} alt='piqr.me' />
+                </div>
+                <div className='bottom'>
+                    <a href="https://piqr.me" target="_blank" rel="noreferrer">piqr.me</a>
+                    <div className='subtitle'>Photo sharing made safe</div>
                 </div>
             </div>
         </div>
